b7800bfef25813d27436dc9c3d8db49e9d490bd2
mastachef <80359894+mastachef@users.noreply.github.com>
2021-05-18 15:08:56 -0700

    Initial commit

---
+++ precache-manifest.25e7765617d914f06850c4934a86248d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5f74ad47eaa5174014405d0059689a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "134cb5da60232aa28373",
+    "url": "/static/js/2.01babe5c.chunk.js"
+  },
+  {
+    "revision": "fd49e39baa7e51df7ef36cc0c3a87b9b",
+    "url": "/static/js/2.01babe5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63d45dbe05039c9cae9",
+    "url": "/static/js/main.2dd9aba1.chunk.js"
+  },
+  {
+    "revision": "3b87b41795ebae894b70",
+    "url": "/static/js/runtime-main.784a1eda.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "96aa39d45580389370bdb45a64a3c4e8",
+    "url": "/static/media/logo-white.96aa39d4.png"
+  },
+  {
+    "revision": "27f913b5d00706bf37d6f98ef3575330",
+    "url": "/static/media/logo.27f913b5.png"
+  }
+]);
